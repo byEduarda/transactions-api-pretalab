b871956932364e9a7a11dc40ca9806603ff377f7
byEduarda <albuquerque.zxl@gmail.com>
2025-09-02 22:21:27 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import { connectDB } from "./database/mongoConnect";
 
@@ -7,14 +8,18 @@ import transactionsRoutes from "./routes/transactionsRoutes";
 import purchasesRoutes from "./routes/purchasesRoutes";
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/purchases", purchasesRoutes);
 app.use("/api/products", productsRoutes);
 app.use("/api/transactions", transactionsRoutes);
 
+app.get('/', (_req, res) => {
+  res.json({ message: 'Transactions API v2.1' });
+});
 
 connectDB();
 
