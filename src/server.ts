c7ebc2316c2672daa1db4314a190e98238acb5ef
byEduarda <albuquerque.zxl@gmail.com>
2025-09-02 22:56:23 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
-import app from "./app";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+import app from "./app";
+
 dotenv.config();
 
-import { connectDB } from "./database/mongoConnect";
-
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+if (!MONGO_URI) {
+  console.error("MONGO_URI não definido!");
+  process.exit(1);
+}
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB conectado com sucesso!");
+    app.listen(PORT, () => {
+      console.log(`Server rodando na porta ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("Erro ao conectar no MongoDB:", err);
+    process.exit(1);
   });
-});
