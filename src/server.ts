bdb9328d3bebe47e69c4aaffb9ab63fb817b846d
byEduarda <albuquerque.zxl@gmail.com>
2025-09-02 22:21:30 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,12 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { connectDB } from "./database/mongoConnect";
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+});
