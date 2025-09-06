77f248d4d1c6597cf6529dc571a6701be9193861
byEduarda <albuquerque.zxl@gmail.com>
2025-09-06 11:47:10 -0300

    Update purchaseService.ts

--- src/services/purchaseService.ts
+++ src/services/purchaseService.ts
@@ -31,19 +31,25 @@ export class PurchaseService {
   }
 
   async create(purchase: CreatePurchaseDTO): Promise<Purchase> {
-    if (purchase.total > 20000) {
-      throw new Error("O valor total da compra excede o limite de R$20.000.");
-    }
-
-    const count = await PurchaseModel.countDocuments();
-    const nextId = (count + 1).toString();
-
-    const newPurchase = new PurchaseModel({
-      ...purchase,
-      id: nextId,
-      date: new Date(),
-    });
-
-    return newPurchase.save();
+  if (purchase.total > 20000) {
+    throw new Error("O valor total da compra excede o limite de R$20.000.");
   }
+
+  const count = await PurchaseModel.countDocuments();
+  const nextId = (count + 1).toString();
+
+  const newPurchase = new PurchaseModel({
+    id: nextId,
+    date: new Date(),
+    total: purchase.total,
+    items: purchase.items.map(item => ({
+      productId: item.productId,
+      name: item.name,
+      price: item.price,
+      quantity: item.quantity,
+    })),
+  });
+
+  return newPurchase.save();
+}
 }
