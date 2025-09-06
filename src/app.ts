ca75e176d5a0eece406f7123c26aa1db51ae5313
byEduarda <albuquerque.zxl@gmail.com>
2025-09-06 19:26:16 -0300

    Revert "rota de chat AI"
    This reverts commit e1585adb25472a43259ab02497c586d49bc9982f.

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,11 @@ import transactionsRoutes from "./routes/transactionsRoutes";
 import purchasesRoutes from "./routes/purchasesRoutes";
 import checkoutRoutes from "./routes/checkoutRoutes";
 import syncRoutes from "./routes/syncRoutes";
-import aiRoutes from "./routes/aiRoutes";
-
-import { chat } from './controllers/aiController';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
 app.use("/purchases", purchasesRoutes);
@@ -20,9 +18,6 @@ app.use("/checkout", checkoutRoutes);
 app.use("/products", productsRoutes);
 app.use("/transactions", transactionsRoutes);
 app.use("/sync-products", syncRoutes);
-app.use('/chat', aiRoutes);
-
-app.post('/chat', chat);
 
 app.get('/', (_req, res) => {
   res.json({ message: 'Transactions API v2.1' });
--- src/routes/aiRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { chat } from "../controllers/aiController";
-
-const router = Router();
-router.post("/chat", chat);
-
-export default router;
