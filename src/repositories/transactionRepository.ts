461d80666d9ffef1f62ddf6be81220ac5aef21cf
byEduarda <albuquerque.zxl@gmail.com>
2025-09-06 11:57:27 -0300

    Update transactionRepository.ts

--- src/repositories/transactionRepository.ts
+++ src/repositories/transactionRepository.ts
@@ -1,27 +1,25 @@
 import { Transaction, transactions as initialTransactions } from "../models/Transactions";
-import { v4 as uuid } from "uuid";
 
 export class TransactionRepository {
   private transactions: Transaction[] = [...initialTransactions];
 
-
   getAll(): Transaction[] {
     return [...this.transactions].sort((a, b) => {
-      const dateA = new Date(a.date ?? 0).getTime();
-      const dateB = new Date(b.date ?? 0).getTime();
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
       return dateB - dateA;
     });
   }
 
   getById(id: string): Transaction | undefined {
-    return this.transactions.find((transaction) => transaction.id === id);
+    return this.transactions.find(transaction => transaction.id === id);
   }
 
   create(transaction: Omit<Transaction, "id">): Transaction {
     const newTransaction: Transaction = {
-      id: uuid(),
+      id: (this.transactions.length + 1).toString(), 
       ...transaction,
-      date: transaction.date ?? new Date().toISOString(), 
+      date: transaction.date ?? new Date().toISOString(),
     };
 
     this.transactions.push(newTransaction);
